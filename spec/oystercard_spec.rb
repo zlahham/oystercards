f974d00204895748d74eb0d781025e1eaaa64b83
Zaid Al Lahham <zlahham@gmail.com>
2016-01-06 13:08:33 +0000

    refactored test to use subject

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,8 +2,7 @@ require "oystercard"
 
 describe Oystercard do
   it 'that are NEW should have a balance of zero' do
-    oyster = Oystercard.new
-    expect(oyster.balance).to eq 0
+    expect(subject.balance).to eq 0
   end
 
 end
