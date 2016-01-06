3b2bfec4786547981a8d8772308458841738c8b7
Zaid Al Lahham <zlahham@gmail.com>
2016-01-06 17:11:49 +0000

    touch in shows the station name

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,15 +1,15 @@
 class Oystercard
-  attr_reader :balance
+  attr_reader :balance, :entry_station
 
   BALANCE_UPPER_LIMIT = 90
   BALANCE_LOWER_LIMIT = 0
   BALANCE_BARRIER_LIMIT = 1
   JOURNEY_PRICE = 2
-  INITIAL_JOURNEY_STATUS = false
+  # STATION_NAMES = []
 
   def initialize
     @balance = BALANCE_LOWER_LIMIT
-    @in_journey = INITIAL_JOURNEY_STATUS
+    @entry_station = nil
   end
 
   def top_up(ammount)
@@ -19,19 +19,19 @@ class Oystercard
 
 
   def in_journey?
-    @in_journey
+    !!entry_station
   end
 
-  def touch_in
+  def touch_in(station)
     fail 'You cannot touch in again if you are in a journey' if in_journey? == true
     fail "You cannot touch in if your balance is less than #{BALANCE_BARRIER_LIMIT}" if balance < BALANCE_BARRIER_LIMIT
-    @in_journey = !INITIAL_JOURNEY_STATUS
+    @entry_station = station
   end
 
   def touch_out
     fail 'You cannot touch out if you are not in a journey' if in_journey? == false
-    @in_journey = INITIAL_JOURNEY_STATUS
     deduct(JOURNEY_PRICE)
+    @entry_station = nil
   end
 
   private
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,6 +7,7 @@ describe Oystercard do
   let(:journey_status)  { Oystercard::INITIAL_JOURNEY_STATUS }
   let(:journey_price)   { Oystercard::JOURNEY_PRICE }
   let(:test_numbers)    { [5, 0.5] }
+  let(:station){ double :station }
 
   it 'that are NEW should have a balance of zero' do
     expect(subject.balance).to eq min_card_balance
@@ -37,19 +38,25 @@ describe Oystercard do
       describe "#touch_in" do
         it 'should be true when #touch_in' do
           subject.top_up(max_card_balance)
-          subject.touch_in
+          subject.touch_in(station)
           expect(subject).to be_in_journey
         end
 
         it 'raises error if #touch_in is with a previous #touch_in' do
           subject.top_up(max_card_balance)
-          subject.touch_in
-          expect { subject.touch_in }.to raise_error("You cannot touch in again if you are in a journey")
+          subject.touch_in(station)
+          expect { subject.touch_in(station) }.to raise_error("You cannot touch in again if you are in a journey")
         end
 
         it 'raises an error if balance is less than minimum barrier balance' do
           subject.top_up(test_numbers[1])
-          expect { subject.touch_in }.to raise_error("You cannot touch in if your balance is less than #{barrier_balance}")
+          expect { subject.touch_in(station) }.to raise_error("You cannot touch in if your balance is less than #{barrier_balance}")
+        end
+
+        it 'remembers the name of the station' do
+          subject.top_up(max_card_balance)
+          subject.touch_in(station)
+          expect(subject.entry_station).to eq(station)
         end
       end
 
@@ -57,7 +64,7 @@ describe Oystercard do
         before { subject.top_up(max_card_balance) }
 
         it 'should be false when #touch_out' do
-          subject.touch_in
+          subject.touch_in(station)
           subject.touch_out
           expect(subject).not_to be_in_journey
         end
@@ -67,7 +74,7 @@ describe Oystercard do
         end
 
         it 'changes the balance according to the journey cost' do
-          subject.touch_in
+          subject.touch_in(station)
           expect { subject.touch_out }.to change{ subject.balance }.by(-journey_price)
         end
       end
